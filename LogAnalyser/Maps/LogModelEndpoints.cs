3c6df5ebb8295ea702a75689dcd319698f4351c3
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-15 20:05:15 -0300

    feat: create get all logs endpoint

---
+++ LogAnalyser/Maps/LogModelEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using LogAnalyser.PersistenceContracts;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace LogAnalyser.Maps;
+
+public static class LogModelEndpoints
+{
+    public static void MapLogModelEndpoints(this IEndpointRouteBuilder routes)
+    {
+        routes.MapGet("/api/logs",
+            async ([FromServices] ILogAnalyserContext logAnalyserContext) =>
+            {
+                return await logAnalyserContext.Log.Find(_ => true).ToListAsync();
+            });
+    }
+}
