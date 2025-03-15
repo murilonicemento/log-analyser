c5beb2c4fe98f1ee1fbf3d416047287c9f44e265
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-15 20:00:50 -0300

    feat: create db context

--- LogAnalyser.Persistence/Configuration/LogAnalyserConfiguration.cs
+++ LogAnalyser.Persistence/Configuration/LogAnalyserConfiguration.cs
@@ -2,5 +2,22 @@
 
 public class LogAnalyserConfiguration
 {
-    
+    public string Database { get; set; }
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string User { get; set; }
+    public string Password { get; set; }
+
+    public string ConnectionString
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(User) || string.IsNullOrEmpty(Password))
+            {
+                return $"mongodb://{Host}:{Port}";
+            }
+
+            return $"mongodb://{User}:{Password}@{Host}:{Port}";
+        }
+    }
 }
---
+++ LogAnalyser.Persistence/LogAnalyserContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using LogAnalyser.Entities;
+using LogAnalyser.Persistence.Configuration;
+using LogAnalyser.PersistenceContracts;
+using MongoDB.Driver;
+
+namespace LogAnalyser.Persistence;
+
+public class LogAnalyserContext : ILogAnalyserContext
+{
+    public IMongoCollection<Log> Log { get; }
+    private readonly IMongoDatabase _database;
+
+    public LogAnalyserContext(IOptions<LogAnalyserConfiguration> configuration)
+    {
+        var client = new MongoClient(configuration.Value.ConnectionString);
+        _database = client.GetDatabase(configuration.Value.Database);
+        Log = _database.GetCollection<Log>("log");
+    }
+}
