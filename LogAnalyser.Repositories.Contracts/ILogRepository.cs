bd0458189a27f07b6f216c64a23280a1c0a33485
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-16 16:50:48 -0300

    feat: add GetLogsCountByPeriod method

--- LogAnalyser.Repositories.Contracts/ILogRepository.cs
+++ LogAnalyser.Repositories.Contracts/ILogRepository.cs
@@ -1,9 +1,13 @@
 ﻿using LogAnalyser.Entities;
+using LogAnalyser.Entities.Enums;
+using MongoDB.Bson;
 
 namespace LogAnalyser.Repositories.Contracts;
 
 public interface ILogRepository
 {
     public Task RegisterLog(Log log);
-    public Task<List<Log>> GetLogsByPeriod(DateTime startDate, DateTime endDate);
+    public Task<List<Log>> GetLogsByPeriod(DateTime startDate, DateTime endDate, LogLevelOptions? logLevel);
+
+    public Task<List<BsonDocument>> GetLogsCountByPeriod(DateTime startDate, DateTime endDate);
 }
