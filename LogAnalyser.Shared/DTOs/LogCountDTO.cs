be2884a73b9dd31a73e1ebba674c862641aed78c
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-16 16:49:44 -0300

    feat: create dto to deserialize bson document

---
+++ LogAnalyser.Shared/DTOs/LogCountDTO.cs
@@ -0,0 +1,11 @@
+﻿using LogAnalyser.Entities.Enums;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LogAnalyser.Shared.DTOs;
+
+public class LogCountDTO
+{
+    public LogLevelOptions Id { get; set; }
+
+    public int Count { get; set; }
+}
