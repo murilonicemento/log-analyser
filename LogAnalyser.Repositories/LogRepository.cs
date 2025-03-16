d6fca7cfcbd5f06fca9fe34c1d10b2fc32472334
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-16 16:51:25 -0300

    feat: implements GetLogsCountByPeriod method

--- LogAnalyser.Repositories/LogRepository.cs
+++ LogAnalyser.Repositories/LogRepository.cs
@@ -1,6 +1,10 @@
 ﻿using LogAnalyser.Entities;
+using LogAnalyser.Entities.Enums;
 using LogAnalyser.Persistence.Contracts;
 using LogAnalyser.Repositories.Contracts;
+using LogAnalyser.Shared.DTOs;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 namespace LogAnalyser.Repositories;
@@ -19,13 +23,38 @@ public class LogRepository : ILogRepository
         await _logAnalyserContext.Log.InsertOneAsync(log);
     }
 
-    public async Task<List<Log>> GetLogsByPeriod(DateTime startDate, DateTime endDate)
+    public async Task<List<Log>> GetLogsByPeriod(DateTime startDate, DateTime endDate, LogLevelOptions? logLevel)
     {
-        var filter = Builders<Log>.Filter.And(
-            Builders<Log>.Filter.Gte(log => log.Timestamp, startDate),
-            Builders<Log>.Filter.Lte(log => log.Timestamp, endDate)
-        );
+        var filter = logLevel is not null
+            ? Builders<Log>.Filter.And(
+                Builders<Log>.Filter.Gte(log => log.Timestamp, startDate),
+                Builders<Log>.Filter.Lte(log => log.Timestamp, endDate),
+                Builders<Log>.Filter.Eq(log => log.LogLevel, logLevel)
+            )
+            : Builders<Log>.Filter.And(
+                Builders<Log>.Filter.Gte(log => log.Timestamp, startDate),
+                Builders<Log>.Filter.Lte(log => log.Timestamp, endDate)
+            );
 
         return await _logAnalyserContext.Log.Find(filter).ToListAsync();
+    }
+
+    public async Task<List<BsonDocument>> GetLogsCountByPeriod(DateTime startDate, DateTime endDate)
+    {
+        var pipeline = new[]
+        {
+            new BsonDocument("$match", new BsonDocument("Timestamp", new BsonDocument
+            {
+                { "$gte", startDate },
+                { "$lte", endDate },
+            })),
+            new BsonDocument("$group", new BsonDocument
+            {
+                { "_id", "$LogLevel" },
+                { "Count", new BsonDocument("$sum", 1) }
+            })
+        };
+
+        return await _logAnalyserContext.Log.Aggregate<BsonDocument>(pipeline).ToListAsync();
     }
 }
