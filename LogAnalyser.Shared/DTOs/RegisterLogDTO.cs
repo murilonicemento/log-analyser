64502f4be862f8572775bf013e1a9a34da4fecde
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-16 13:39:08 -0300

    feat: create register log dto

---
+++ LogAnalyser.Shared/DTOs/RegisterLogDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace LogAnalyser.Shared.DTOs;
+
+public class RegisterLogDTO
+{
+    public DateTime Timestamp { get; set; }
+
+    public string LogLevel { get; set; }
+
+    public string Message { get; set; }
+
+    public string Service { get; set; }
+
+    public string OrigemIP { get; set; }
+
+    public int OperationTime { get; set; }
+}
