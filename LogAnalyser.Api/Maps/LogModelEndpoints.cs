07e0ff2283813e2405d9ca035db99771ba6b45c5
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-16 13:41:03 -0300

    feat: create register log and get logs by period endpoint

--- LogAnalyser.Api/Maps/LogModelEndpoints.cs
+++ LogAnalyser.Api/Maps/LogModelEndpoints.cs
@@ -1,6 +1,9 @@
-﻿using LogAnalyser.PersistenceContracts;
+﻿using LogAnalyser.Api.Validators;
+using LogAnalyser.Entities;
+using LogAnalyser.Entities.Enums;
+using LogAnalyser.Repositories.Contracts;
+using LogAnalyser.Shared.DTOs;
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Driver;
 
 namespace LogAnalyser.Api.Maps;
 
@@ -8,10 +11,46 @@ public static class LogModelEndpoints
 {
     public static void MapLogModelEndpoints(this IEndpointRouteBuilder routes)
     {
-        routes.MapGet("/api/logs",
-            async ([FromServices] ILogAnalyserContext logAnalyserContext) =>
+        routes.MapPost("/api/register",
+            async ([FromBody] RegisterLogDTO registerLogDto, [FromServices] ILogRepository logRepository) =>
             {
-                return await logAnalyserContext.Log.Find(_ => true).ToListAsync();
+                var validator = new RegisterLogValidator();
+                var validationResult = await validator.ValidateAsync(registerLogDto);
+
+                if (!validationResult.IsValid)
+                {
+                    var messages = string.Join(" | ", validationResult.Errors);
+
+                    return Results.Problem(messages, statusCode: 400);
+                }
+
+                var log = new Log
+                {
+                    Timestamp = registerLogDto.Timestamp,
+                    LogLevel = Enum.Parse<LogLevelOptions>(registerLogDto.LogLevel, ignoreCase: true),
+                    Message = registerLogDto.Message,
+                    Service = registerLogDto.Service,
+                    OrigemIP = registerLogDto.OrigemIP,
+                    OperationTime = registerLogDto.OperationTime,
+                };
+
+                await logRepository.RegisterLog(log);
+
+                return Results.Created();
+            });
+
+        routes.MapGet("/api/errorsByPeriod",
+            async ([FromQuery] DateTime startDate, [FromQuery] DateTime endDate,
+                [FromServices] ILogRepository logRepository) =>
+            {
+                if (startDate >= endDate)
+                {
+                    return Results.Problem("Start date must be less then end date");
+                }
+
+                var logs = await logRepository.GetLogsByPeriod(startDate, endDate);
+
+                return Results.Ok(logs);
             });
     }
 }
