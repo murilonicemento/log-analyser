b128aabba5266a2f4b3022c438ea57f73ebff529
murilonicemento <murilo_nascimento22@outlook.com>
2025-03-16 13:39:56 -0300

    feat: create register log dto validations

---
+++ LogAnalyser.Api/Validators/RegisterLogValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using LogAnalyser.Entities.Enums;
+using LogAnalyser.Shared.DTOs;
+
+namespace LogAnalyser.Api.Validators;
+
+public class RegisterLogValidator : AbstractValidator<RegisterLogDTO>
+{
+    public RegisterLogValidator()
+    {
+        RuleFor(registerLogDto => registerLogDto.Timestamp).NotNull();
+        RuleFor(registerLogDto => registerLogDto.LogLevel)
+            .NotNull()
+            .IsEnumName(typeof(LogLevelOptions), caseSensitive: false)
+            .WithMessage("Log level is not valid.");
+        RuleFor(registerLogDto => registerLogDto.Message).NotNull();
+        RuleFor(registerLogDto => registerLogDto.Service).NotNull();
+        RuleFor(registerLogDto => registerLogDto.OrigemIP).NotNull();
+        RuleFor(registerLogDto => registerLogDto.OperationTime).NotNull().GreaterThanOrEqualTo(1);
+    }
+}
